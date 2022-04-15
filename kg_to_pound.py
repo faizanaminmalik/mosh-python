4cbcd1bf2b2ce01646ebe716e1ba898db2084ab0
faizanaminmalik <fxnmalik@gmail.com>
2022-04-15 07:58:55 +0530

    first commit to creation

---
+++ kg_to_pound.py
@@ -0,0 +1,5 @@
+#Program to convert weight in KG to LB
+
+weight_kg = input('Enter your weight in KG:')
+weight_lbs = int(weight_kg) * 2.205
+print ('Your weight in LBS is', weight_lbs)
---
+++ multiline_string.py
@@ -0,0 +1,11 @@
+text_email = """
+Dear John Does,
+
+Hope you are doing Well!
+
+We're glad that we have got you onboard.
+
+Best Regards,
+The "A" Team
+"""
+print (text_email)
